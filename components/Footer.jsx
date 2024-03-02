77737a8146ff8f3e52ce053ba6e09b71a052253c
Darshan Chobarkar <dchobarkar@gmail.com>
2024-03-02 15:56:18 +0530

    add layout component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2024 JSM Headphones All rights reserverd</p>
+
+      <p className="icons">
+        <AiFillInstagram />
+
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
